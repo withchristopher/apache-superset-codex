apache/superset

fix(export): sanitize control characters in titles to prevent export failures
Dashboard/chart titles containing non-printable control characters
(e.g., U+0008 backspace) cause headless browsers (Playwright/Chromium)
to hang or crash during screenshot/PDF export.

Changes:
- Add sanitize_title() to superset/utils/file.py that strips C0/C1
  control characters (except tab, LF, CR)
- Apply sanitization in get_filename() for export filenames
- Sanitize report notification names in execute.py
- Add frontend sanitizeDocumentTitle() utility for document.title
- Apply sanitization in DashboardPage and ExploreViewContainer
- Add comprehensive tests for both Python and TypeScript

Fixes #38992

Made-with: Cursor

superset-frontend/src/dashboard/containers/DashboardPage.tsx
@@ -41,6 +41,7 @@ import { getActiveFilters } from 'src/dashboard/util/activeDashboardFilters';
 import { LocalStorageKeys, setItem } from 'src/utils/localStorageHelpers';
 import { URL_PARAMS } from 'src/constants';
 import { getUrlParam } from 'src/utils/urlUtils';
+import { sanitizeDocumentTitle } from 'src/utils/sanitizeDocumentTitle';
 import { setDatasetsStatus } from 'src/dashboard/actions/dashboardState';
 import {
   getFilterValue,
@@ -233,7 +234,7 @@ export const DashboardPage: FC<PageProps> = ({ idOrSlug }: PageProps) => {
   // Update document title when dashboard title changes
   useEffect(() => {
     if (dashboard_title) {
-      document.title = dashboard_title;
+      document.title = sanitizeDocumentTitle(dashboard_title);
     }
   }, [dashboard_title]);
 

withchristopher/apache-superset-codex

superset-frontend/src/dashboard/containers/DashboardPage.tsx
@@ -42,6 +42,7 @@ import { getActiveFilters } from 'src/dashboard/util/activeDashboardFilters';
 import { LocalStorageKeys, setItem } from 'src/utils/localStorageHelpers';
 import { URL_PARAMS } from 'src/constants';
 import { getUrlParam } from 'src/utils/urlUtils';
+import { sanitizeDocumentTitle } from 'src/utils/sanitizeDocumentTitle';
 import { setDatasetsStatus } from 'src/dashboard/actions/dashboardState';
 import { DASHBOARD_HEADER_ID } from 'src/dashboard/util/constants';
 import {
@@ -322,7 +323,7 @@ export const DashboardPage: FC<PageProps> = ({ idOrSlug }: PageProps) => {
   // Update document title when dashboard title changes
   useEffect(() => {
     if (pageTitle) {
-      document.title = pageTitle;
+      document.title = sanitizeDocumentTitle(pageTitle);
     }
   }, [pageTitle]);
 